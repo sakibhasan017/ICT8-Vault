8c8b00c4c7695ebc1861b086f5249088a5a6fa26
sakibhasan017 <hassansakib512@gmail.com>
2025-08-09 17:55:06 +0600

    Modify Cron

--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,7 @@ app.use("/api/test",testRoute);
 
 
 cron.schedule('0 15 * * *', () => {
-  console.log("Running 4.37PM notifier...");
+  console.log("Running 3.00PM notifier...");
   sendScheduledNotifications();
 });
 
